4aae5345beccf71522f60c0e4e9766acfa1d17c6
Miftaq-PC <bagusmiftaq>
2019-12-10 19:53:51 +0700

    Second Commit

--- app/src/main/java/com/edicoding/picodiploma/rongsokinuser/ProfileFragment.java
+++ app/src/main/java/com/edicoding/picodiploma/rongsokinuser/ProfileFragment.java
@@ -1,22 +1,43 @@
 package com.edicoding.picodiploma.rongsokinuser;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.navigation.fragment.NavHostFragment;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 public class ProfileFragment extends NavHostFragment {
+
+    FirebaseAuth fAuth;
+
+    Button btnExit;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_profile, container, false);
-        
-    return v;
-    }
 
+        btnExit = v.findViewById(R.id.btn_exit);
+        fAuth = FirebaseAuth.getInstance();
+
+        btnExit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                fAuth.signOut();
+                
+                Intent goexit = new Intent(getActivity(), Login_Act.class);
+                startActivity(goexit);
+            }
+        });
+
+        return v;
+    }
 }
